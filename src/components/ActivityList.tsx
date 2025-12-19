e94417b501b3c71f2fe6fef849500305da168e44
bmenc <bmenchaca.cca@gmail.com>
2025-12-19 15:58:34 -0600

    152. Creando la acción para seleccionar la actividad a editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,13 +1,15 @@
-import { useMemo } from "react";
+import { useMemo, type Dispatch } from "react";
 import type { Activity } from "../types";
 import { categories } from "../data/categories";
 import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import type { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
-  activities: Activity[]
+  activities: Activity[],
+  dispatch: Dispatch<ActivityActions>
 }
 
-export default function ActivityList({ activities }: ActivityListProps) {
+export default function ActivityList({ activities, dispatch }: ActivityListProps) {
   const categoryName = useMemo(() =>
     (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
     , [activities])
@@ -28,8 +30,8 @@ export default function ActivityList({ activities }: ActivityListProps) {
             </p>
           </div>
           <div className="flex gap-5 items-center">
-            <button>
-              <PencilSquareIcon className="h-8 w-8 text-gray-800"/>
+            <button onClick={() => dispatch({type: 'set-activeId', payload: {id: activity.id}})}>
+              <PencilSquareIcon className="h-8 w-8 text-gray-800" />
             </button>
           </div>
         </div>
