eb622febd911962894cfde1dbbe45e4dbb031c36
bmenc <bmenchaca.cca@gmail.com>
2025-12-20 02:04:14 -0600

    157. Creando el botón para llamar la acción de eliminar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { useMemo, type Dispatch } from "react";
 import type { Activity } from "../types";
 import { categories } from "../data/categories";
-import { PencilSquareIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, XCircleIcon} from '@heroicons/react/24/outline';
 import type { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
@@ -32,6 +32,9 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
           <div className="flex gap-5 items-center">
             <button onClick={() => dispatch({type: 'set-activeId', payload: {id: activity.id}})}>
               <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+            </button>
+            <button onClick={() => dispatch({type: 'delete-activity', payload: {id: activity.id}})}>
+              <XCircleIcon className="h-8 w-8 text-red-500" />
             </button>
           </div>
         </div>
