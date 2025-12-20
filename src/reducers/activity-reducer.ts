34e20b861b23b533604719289febf3158d490b44
bmenc <bmenchaca.cca@gmail.com>
2025-12-20 00:48:40 -0600

    155. Actualizando las actividades

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,7 +2,7 @@ import type { Activity } from "../types"
 
 export type ActivityActions =
   { type: 'save-activity', payload: { newActivity: Activity } } |
-  { type: 'set-activeId', payload: { id: Activity['id'] } } 
+  { type: 'set-activeId', payload: { id: Activity['id'] } }
 
 export type ActivityState = {
   activities: Activity[],
@@ -20,15 +20,24 @@ export const activityReducer = (
 ) => {
   switch (action.type) {
     case "save-activity": {
+
+      let updatedActivities: Activity[] = []
+      if (state.activeId) {
+        updatedActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+      } else {
+        updatedActivities = [...state.activities, action.payload.newActivity]
+      }
+
       return {
         ...state,
-        activities: [...state.activities, action.payload.newActivity],
+        activities: updatedActivities,
+        activeId: ''
       };
     }
     case "set-activeId": {
       return {
         ...state,
-        activeId:  action.payload.id,
+        activeId: action.payload.id,
       };
     }
 
